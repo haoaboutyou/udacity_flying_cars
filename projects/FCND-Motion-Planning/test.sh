81f858c94238078f85cde7ec49645b9e5130a34a
david <haoaboutyou88@gmail.com>
2020-01-12 18:21:44 +1300

    Latest locations

--- projects/FCND-Motion-Planning/test.sh
+++ projects/FCND-Motion-Planning/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# collinear
+# Test that goes to three positions and then heads home.
+ 
 # Go A
-python motion_planning.py --target_lon -122.392250 --target_lat 37.793350 
-
 python motion_planning.py --target_lon -122.4 --target_lat 37.795956 
+
+#python motion_planning.py --target_lon -122.39769 --target_lat 37.793638
 
 # Go B
 python motion_planning.py --target_lon -122.394827 --target_lat 37.793863 
